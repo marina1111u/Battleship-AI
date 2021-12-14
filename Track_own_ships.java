e4837739ccd791a89aeac2d44350a542a94efb42
Bottle-o-Mercury <56655648+Bottle-o-Mercury@users.noreply.github.com>
2021-12-14 09:55:59 -0500

    The updated Tracking
    err, the last part was commented out

--- Track_own_ships.java
+++ Track_own_ships.java
@@ -4,62 +4,93 @@ import java.util.Scanner;
 public class Track_own_ships {
     Scanner cs = new Scanner(System.in);
 
-    public void Track_own_ships(int[][] our_Ships, boolean turn, object) {
+//change each ship to be differented via numbers
+    /*
+    Destroyer = index 0s, length of 2
+    Submarine = index 3s, length of 3
+    Cruiser = index 4s, length of 3
+    Battleship = index 5s, length of 4
+    Carrier = index 6s, length of 5
+     */
+
+    public Track_own_ships(int[][] our_Ships, boolean turn) {
 
         //enemy shots
-        int rows=cs.nextInt();
-        int columns=cs.nextInt();
+        int rows = cs.nextInt();
+        int columns = cs.nextInt();
 
-        //int enemy_Shots[][]=new int[rows][columns];
+        int enemy_Shots[][] = new int[rows][columns];
 
-        if(our_Ships [rows][columns] == 0){
-            System.out.println("Oh no, you hit our " + object);
+
+        if (our_Ships[rows][columns] == 0) {
+            System.out.println("Oh no, you hit our destroyer"); //destroyer
             //music here - Kaboom.
-            our_Ships [rows][columns] = 2; //setting the index as 'hit, and now injured
+            our_Ships[rows][columns] = -9; //setting the index as 'hit, and now injured
             turn = !turn;
-        }
-
-        else if(object.alive = true){
-            //all set all array values in
+        } else if (our_Ships[rows][columns] == -9) { //just for our reference - we don't tell the enemy they hit us twice
+            System.out.println("Oh no, you hit our destroyer"); //destroyer
+            //music here - Kaboom.
             turn = !turn;
-        }
-
-        else if(our_Ships[rows][columns] == 2) {
-            System.out.println("Oh no- wait. You already hit our " +object);
-            //music here - kaboom?
-            if (turn){
-                turn = false;
-            }
-            else{
-                turn = true;
-            }
-
-            /*note:
-                       if (turn){
-                           turn = false;
-                        }
-                        else{
-                            turn = true;
-                  }
-
-                  is the same as turn = !turn
-
-
-             */
-
-        }
-
-        else{
+        } else if (our_Ships[rows][columns] == 3) {
+            System.out.println("Oh no, you hit our submarine");
+            //music here - Kaboom.
+            our_Ships[rows][columns] = -3; //setting the index as 'hit, and now injured
+            turn = !turn;
+        } else if (our_Ships[rows][columns] == -3) { //just for our reference - we don't tell the enemy they hit us twice
+            System.out.println("Oh no, you hit our submarine");
+            //music here - Kaboom.
+            turn = !turn;
+        } else if (our_Ships[rows][columns] == 4) {
+            System.out.println("Oh no, you hit our cruiser"); //cruiser
+            //music here - Kaboom.
+            our_Ships[rows][columns] = -4; //setting the index as 'hit, and now injured
+            turn = !turn;
+        } else if (our_Ships[rows][columns] == -4) { //just for our reference - we don't tell the enemy they hit us twice
+            System.out.println("Oh no, you hit our cruiser"); //cruiser
+            //music here - Kaboom.
+            turn = !turn;
+        } else if (our_Ships[rows][columns] == 5) {
+            System.out.println("Oh no, you hit our battleship"); //battleship
+            //music here - Kaboom.
+            our_Ships[rows][columns] = -5; //setting the index as 'hit, and now injured
+            turn = !turn;
+        } else if (our_Ships[rows][columns] == -5) { //just for our reference - we don't tell the enemy they hit us twice
+            System.out.println("Oh no, you hit our battleship"); //battleship
+            //music here - Kaboom.
+            turn = !turn;
+        } else if (our_Ships[rows][columns] == 6) {
+            System.out.println("Oh no, you hit our carrier"); //carrier
+            //music here - Kaboom.
+            our_Ships[rows][columns] = -6; //setting the index as 'hit, and now injured
+            turn = !turn;
+        } else if (our_Ships[rows][columns] == -6) { //just for our reference - we don't tell the enemy they hit us twice
+            System.out.println("Oh no, you hit our carrier"); //carrier
+            //music here - Kaboom.
+            turn = !turn;
+        } else if (our_Ships[rows][columns] == 1) {
             System.out.println("Lol, you missed.");
             //music here - lmao
             turn = !turn;
-
         }
+
+        /*
+        if (our_Ships has a row / columns of adjact negative numbers, set that match the assigned length and
+        assigned negative value: ex.a pair of - 9 s in a row/columns = mark those negative number set as 1){
+            //all set all array values in the array as 1
+            turn = !turn;
+        }
+         */
+
         System.out.println("Okay, my turn." );
 
     }
 
-    }
+}
+
+
+
+
+
 
 
 
